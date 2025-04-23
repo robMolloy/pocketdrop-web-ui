35da1754efcc6ac364d970483bfafc5fc3073859
robmolloy <robmolloy@hotmail.co.uk>
2025-04-23 23:22:01 +0100

    starred page  - remove unused actions and open fileDetails on row click

--- src/pages/starred.tsx
+++ src/pages/starred.tsx
@@ -1,8 +1,7 @@
-import { FileIcon, getFileExtension } from "@/components/FileIcon";
 import { FileDetails } from "@/components/FileDetails";
+import { FileIcon, getFileExtension } from "@/components/FileIcon";
 import { RightSidebarContent } from "@/components/RightSidebar";
 import { ToggleableStar } from "@/components/ToggleableStar";
-import { Button } from "@/components/ui/button";
 import {
   Table,
   TableBody,
@@ -13,9 +12,6 @@ import {
 } from "@/components/ui/table";
 import { useFilesStore } from "@/modules/files/filesStore";
 import { useRightSidebarStore } from "@/stores/rightSidebarStore";
-import { Download, Eye } from "lucide-react";
-import { pb } from "@/config/pocketbaseConfig";
-import { getFile, downloadFile } from "@/modules/files/dbFilesUtils";
 
 const StarredPage = () => {
   const filesStore = useFilesStore();
@@ -39,7 +35,7 @@ const StarredPage = () => {
             <TableHead>Path</TableHead>
             <TableHead>Type</TableHead>
             <TableHead>Created</TableHead>
-            <TableHead>Actions</TableHead>
+            <TableHead></TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
@@ -48,7 +44,16 @@ const StarredPage = () => {
             const directoryPath = file.filePath.substring(0, file.filePath.lastIndexOf("/"));
 
             return (
-              <TableRow key={file.id}>
+              <TableRow
+                key={file.id}
+                onClick={() => {
+                  rightSidebarStore.setData(
+                    <RightSidebarContent title="File Details">
+                      <FileDetails file={file} onDelete={() => rightSidebarStore.close()} />
+                    </RightSidebarContent>,
+                  );
+                }}
+              >
                 <TableCell className="flex items-center gap-2">
                   <FileIcon extension={getFileExtension(file)} size={24} />
                   <span>{fileName}</span>
@@ -58,29 +63,6 @@ const StarredPage = () => {
                 <TableCell>{formatDate(file.created)}</TableCell>
                 <TableCell>
                   <div className="flex items-center gap-2">
-                    <Button
-                      variant="ghost"
-                      size="icon"
-                      onClick={() => {
-                        rightSidebarStore.setData(
-                          <RightSidebarContent title="File Details">
-                            <FileDetails file={file} onDelete={() => rightSidebarStore.close()} />
-                          </RightSidebarContent>,
-                        );
-                      }}
-                    >
-                      <Eye className="h-4 w-4" />
-                    </Button>
-                    <Button
-                      variant="ghost"
-                      size="icon"
-                      onClick={async () => {
-                        const resp = await getFile({ pb, id: file.id, isThumb: false });
-                        if (resp.success) downloadFile({ data: resp.data });
-                      }}
-                    >
-                      <Download className="h-4 w-4" />
-                    </Button>
                     <ToggleableStar file={file} size="sm" />
                   </div>
                 </TableCell>
