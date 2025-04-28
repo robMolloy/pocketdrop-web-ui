22902be10d3f8cf824cc4bbd09d1bc35726bf81c
robmolloy <robmolloy@hotmail.co.uk>
2025-04-28 21:38:06 +0100

    starred page cleanup and use viewTypeStore

--- src/pages/starred.tsx
+++ src/pages/starred.tsx
@@ -1,20 +1,39 @@
+import { DisplayDirectoriesAndFilesIconView } from "@/modules/directories/components/DisplayDirectoriesAndFilesIconView";
 import { useDirectoryTreeStore } from "@/modules/files/directoriesStore";
 import { DisplayDirectoriesAndFilesTableView } from "@/modules/files/DisplayFilesTableView";
 import { useFilesStore } from "@/modules/files/filesStore";
+import { ViewTypeToggleButton } from "@/modules/viewType/components/ViewTypeToggleButton";
+import { useViewTypeStore } from "@/modules/viewType/viewTypeStore";
 
 const StarredPage = () => {
   const filesStore = useFilesStore();
   const starredFiles = filesStore.data?.filter((file) => file.isStarred) ?? [];
   const directoriesStore = useDirectoryTreeStore();
+  const viewTypeStore = useViewTypeStore();
+
   return (
     <div>
-      <h1 className="mb-4 text-3xl font-bold">Starred Files</h1>
+      <div className="flex items-end justify-between">
+        <h1 className="mb-0 text-2xl font-bold">Starred Files</h1>
+        <ViewTypeToggleButton />
+      </div>
 
-      <DisplayDirectoriesAndFilesTableView
-        directories={[]}
-        files={starredFiles}
-        parentDirectories={directoriesStore.fullPaths ?? []}
-      />
+      <br />
+      {viewTypeStore.data === "table" && (
+        <DisplayDirectoriesAndFilesTableView
+          directories={[]}
+          files={starredFiles}
+          parentDirectories={directoriesStore.fullPaths ?? []}
+        />
+      )}
+
+      {viewTypeStore.data === "icon" && (
+        <DisplayDirectoriesAndFilesIconView
+          directories={[]}
+          files={starredFiles}
+          parentDirectories={directoriesStore.fullPaths ?? []}
+        />
+      )}
     </div>
   );
 };
