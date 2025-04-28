37f1813fb01a8078e89a6d41764ab54c2649a6c3
robmolloy <robmolloy@hotmail.co.uk>
2025-04-28 10:21:31 +0100

    Add directory and file name uniqueness validation on update requests

--- pocketbase-leader/pb_hooks/main.pb.js
+++ pocketbase-leader/pb_hooks/main.pb.js
@@ -23,7 +23,31 @@ onRecordCreateRequest((e) => {
   e.next();
 }, "directories");
 
+onRecordUpdateRequest((e) => {
+  const directoryRelationId = e.record.get("directoryRelationId");
+  const name = e.record.get("name");
+
+  const count = $app.countRecords("directories", $dbx.hashExp({ directoryRelationId, name }));
+
+  if (count > 0)
+    throw new BadRequestError("Two directories within the same parent directory cannot share identical names.");
+
+  e.next();
+}, "directories");
+
 onRecordCreateRequest((e) => {
+  const directoryRelationId = e.record.get("directoryRelationId");
+  const name = e.record.get("name");
+
+  const count = $app.countRecords("files", $dbx.hashExp({ directoryRelationId, name }));
+
+  if (count > 0)
+    throw new BadRequestError("Two files within the same parent directory cannot share identical names.");
+    
+
+  e.next();
+}, "files");
+onRecordUpdateRequest((e) => {
   const directoryRelationId = e.record.get("directoryRelationId");
   const name = e.record.get("name");
 
