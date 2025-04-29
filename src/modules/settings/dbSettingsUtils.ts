e330288a5abcb71d6f40d4d1b9cc3d9ece01e451
robmolloy <robmolloy@hotmail.co.uk>
2025-04-30 00:09:59 +0100

    create dbSettingsUtils for listening

---
+++ src/modules/settings/dbSettingsUtils.ts
@@ -0,0 +1,64 @@
+import PocketBase from "pocketbase";
+import { z } from "zod";
+
+const settingsRecordSchema = z.object({
+  collectionId: z.string(),
+  collectionName: z.string(),
+  id: z.string(),
+  settingName: z.string(),
+  isEnabled: z.boolean(),
+  created: z.string(),
+  updated: z.string(),
+});
+
+export type TSettingsRecord = z.infer<typeof settingsRecordSchema>;
+
+export const listSettings = async (p: { pb: PocketBase }) => {
+  try {
+    const initData = await p.pb.collection("settings").getFullList({
+      sort: "-created",
+    });
+
+    const data = initData
+      .map((x) => settingsRecordSchema.safeParse(x))
+      .filter((x) => x.success)
+      .map((x) => x.data);
+    return { success: true, data } as const;
+  } catch (error) {
+    return { success: false, error } as const;
+  }
+};
+
+export const smartSubscribeToSettings = async (p: {
+  pb: PocketBase;
+  onChange: (x: TSettingsRecord[]) => void;
+}) => {
+  const listSettingsResp = await listSettings(p);
+  if (!listSettingsResp.success) return listSettingsResp;
+
+  let allSettings = listSettingsResp.data;
+  p.onChange(allSettings);
+
+  const unsub = p.pb.collection("settings").subscribe("*", (e) => {
+    if (e.action === "create") {
+      const parseResp = settingsRecordSchema.safeParse(e.record);
+      if (parseResp.success) allSettings.push(parseResp.data);
+    }
+    if (e.action === "update") {
+      const parseResp = settingsRecordSchema.safeParse(e.record);
+      if (!parseResp.success) return;
+
+      allSettings = allSettings.filter((x) => parseResp.data?.id !== x.id);
+      allSettings.push(parseResp.data);
+    }
+    if (e.action === "delete") {
+      const parseResp = settingsRecordSchema.safeParse(e.record);
+      if (!parseResp.success) return;
+
+      allSettings = allSettings.filter((x) => parseResp.data?.id !== x.id);
+    }
+    p.onChange(allSettings);
+  });
+
+  return { success: true, data: unsub } as const;
+};
