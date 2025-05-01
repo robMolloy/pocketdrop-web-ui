49d9d064ea3510a13b931abf7ed17020b6e0e5a4
robmolloy <robmolloy@hotmail.co.uk>
2025-05-01 12:27:36 +0100

    create ScrollContainer component

--- src/pages/ai-chat.tsx
+++ src/pages/ai-chat.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import {
   callClaude,
   TChatMessageContentImageSchema as chatMessageContentImageSchema,
@@ -10,7 +11,7 @@ import {
   DisplayChatMessages,
   ErrorMessage,
 } from "@/modules/aiChat/components/Messages";
-import { useEffect, useRef, useState } from "react";
+import { DependencyList, useEffect, useRef, useState } from "react";
 import { z } from "zod";
 
 const uuid = () => crypto.randomUUID();
@@ -52,22 +53,35 @@ const convertFilesToFileDetails = async (files: File[]) => {
     .map((x) => x.data);
 };
 
+const ScrollContainer = (p: {
+  children: React.ReactNode;
+  deps: DependencyList;
+  className?: string;
+}) => {
+  const scrollContainer = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!scrollContainer.current) return;
+    scrollContainer.current.scrollTop = scrollContainer.current.scrollHeight;
+  }, p.deps);
+
+  return (
+    <div className={cn("overflow-y-auto", p.className)} ref={scrollContainer}>
+      {p.children}
+    </div>
+  );
+};
+
 const AiChat = () => {
   const [mode, setMode] = useState<"ready" | "thinking" | "streaming" | "error">("ready");
   const [messages, setMessages] = useState<TChatMessage[]>([]);
   const [streamedResponse, setStreamedResponse] = useState("");
   const [currentInput, setCurrentInput] = useState("");
   const [currentImages, setCurrentImages] = useState<File[]>([]);
-  const scrollContainer = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (!scrollContainer.current) return;
-    scrollContainer.current.scrollTop = scrollContainer.current.scrollHeight;
-  }, [messages, streamedResponse]);
 
   return (
     <div className="flex h-full flex-col gap-4">
-      <div className="flex flex-1 flex-col gap-4 overflow-y-auto" ref={scrollContainer}>
+      <ScrollContainer deps={[messages, streamedResponse]} className="flex flex-1 flex-col gap-4">
         <AssistantMessage>Hello! How can I help you today?</AssistantMessage>
 
         <DisplayChatMessages messages={messages} />
@@ -75,7 +89,7 @@ const AiChat = () => {
         {mode === "thinking" && <p>Thinking...</p>}
         {mode === "streaming" && <AssistantMessage>{streamedResponse}</AssistantMessage>}
         {mode === "error" && <ErrorMessage />}
-      </div>
+      </ScrollContainer>
 
       <form
         onSubmit={async (e) => {
